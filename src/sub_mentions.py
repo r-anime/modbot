22fbf707d257151de5c30ad87615e509d3223d3c
MajehOS <32561001+MajehOS@users.noreply.github.com>
2020-09-02 04:03:36 +1000

    Quotation marks for consistency

--- src/sub_mentions.py
+++ src/sub_mentions.py
@@ -10,15 +10,15 @@ colour_switcher=0
 while 1==1:
     message = None
     for message in reddit.inbox.unread(limit=1):
-        if message.author == 'Sub_Mentions':
+        if message.author == "Sub_Mentions":
             try:
                 message.mark_read() 
                 title = message.subject 
                 desc = message.body
                 desc = desc[:-279]                                              #removes info at the end of the message
-                desc = re.sub(r'\(/r/', '(https://www.reddit.com/r/', desc)    #hyperlinks reddit links
+                desc = re.sub(r"\(/r/", "(https://www.reddit.com/r/", desc)    #hyperlinks reddit links
                 if len(desc) >= 2000:                                         #message length (max for webhook is 2000)                                          
-                    desc=(desc[:1997]+'...')
+                    desc=(desc[:1997]+"...")
                 colour_switcher = colour_switcher + 1                       #breaks up the flow so its easier on the eye, delete if it's not
                 if colour_switcher % 2 == 1:
                     colour = 242424
