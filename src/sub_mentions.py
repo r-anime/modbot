3df6a125a69579f406a633b07b6c734e483452e8
Durinthal <durinthal@gmail.com>
2020-09-06 13:07:31 -0400

    Embed loop in exception handler for automatic restarts

--- src/sub_mentions.py
+++ src/sub_mentions.py
@@ -5,13 +5,12 @@ import praw
 
 import config
 from utils import discord
+from utils.logger import logger
 
 
-reddit = praw.Reddit(**config.SUB_MENTIONS["auth"])  # Requires an account linked to /u/Sub_Mentions
-colour_switcher = 0
+def listen(reddit):
+    colour_switcher = 0
 
-
-while True:
     for message in reddit.inbox.unread(limit=1):
         if message.author != "Sub_Mentions":
             message.mark_read()
@@ -36,10 +35,25 @@ while True:
             "color": colour  # yeah the Australia
         }
 
+        logger.debug(embed_json)
         discord.send_webhook_message(
             {"embeds": [embed_json]},
             channel_webhook_url=config.DISCORD["webhook_sub_mentions"]
         )
         message.mark_read()
 
+    logger.debug("waiting...")
     time.sleep(2.1)
+
+
+if __name__ == "__main__":
+    while True:
+        try:
+            logger.info("Connecting to Reddit...")
+            reddit = praw.Reddit(**config.SUB_MENTIONS["auth"])  # Requires an account linked to /u/Sub_Mentions
+            while True:
+                listen(reddit)
+        except Exception:
+            delay_time = 30
+            logger.exception(f"Encountered an unexpected error, restarting in {delay_time} seconds...")
+            time.sleep(delay_time)
