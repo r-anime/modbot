f77f2862a6f522dcb0797cacb60fa9c574645bec
Durinthal <durinthal@gmail.com>
2022-09-06 22:09:54 -0400

    Add retrieval of posts by date to comment_archive script

--- src/services/user_service.py
+++ src/services/user_service.py
@@ -1,5 +1,5 @@
 from datetime import datetime, timezone
-from typing import Optional
+from typing import Optional, Union
 
 from praw.models.reddit.redditor import Redditor
 from prawcore.exceptions import NotFound
@@ -10,16 +10,24 @@ from data.user_data import UserData, UserModel
 _user_data = UserData()
 
 
-def get_user(username: str) -> Optional[UserModel]:
+def get_user(username: Union[Redditor, str]) -> Optional[UserModel]:
     """Gets a single user from the database, None if they don't exist."""
 
-    return _user_data.get_user(username)
+    if isinstance(username, str):
+        return _user_data.get_user(username)
+    elif isinstance(username, Redditor):
+        return _user_data.get_user(username.name)
 
 
-def add_user(reddit_user: Redditor) -> UserModel:
+def add_user(reddit_user: Union[Redditor, str]) -> UserModel:
     """Parses some basic information for the user and adds them to the database."""
 
     user = UserModel()
+    # Worst case we just have their username, insert that anyway.
+    if isinstance(reddit_user, str):
+        user.username = reddit_user
+        return _user_data.insert(user, error_on_conflict=False)
+
     user.username = reddit_user.name
 
     # If the user's been suspended, the flag will be set.
