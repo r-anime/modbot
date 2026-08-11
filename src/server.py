92d7e8b6b01f80b720c5dc2f09c2027b59a19f7b
Philip Ross <dev@baseballlover723.com>
2026-08-11 01:36:51 -0700

    add logging and unicorn

--- src/server.py
+++ src/server.py
@@ -1,11 +1,16 @@
 from flask import Flask
+import logging
 
 import config_loader
 
 from controllers.ping import ping_bp
 from controllers.ingestion import ingestion_bp
+from utils.logger import logger
 
 app = Flask(__name__)
+
+for logger_name in ("werkzeug", "gunicorn.error", "gunicorn.access"):
+    logging.getLogger(logger_name).handlers = logger.handlers
 
 app.register_blueprint(ping_bp)
 app.register_blueprint(ingestion_bp)
